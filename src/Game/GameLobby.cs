130aa908ab5cceea1971805ded7f87e9459e16b5
Paweł Potępa <potepapawel@gmail.com>
2025-12-06 16:10:34 +0100

    add basic binary lobby parser

---
+++ src/Game/GameLobby.cs
@@ -0,0 +1,56 @@
+﻿namespace BarcodeRevealTool.Game
+{
+
+    /// <summary>
+    /// as of now we wanna just support 1v1 mode, prolly the structure is a bit different for team games, also arcase, 
+    /// would need dig deeper into the lobby file structure
+    /// </summary>
+    internal class GameLobby
+    {
+        private readonly byte[]? bytes;
+
+        private const int PLAYER_TWO_STARTING_INDEX = 0x8D;
+        private const int PLAYER_CHUNK_LENGTH = 0x8D;
+
+        private Player? _p2;
+        private Player? _p1;
+
+        public Player P1
+        {
+            get
+            {
+
+                Span<byte> p1 = new Span<byte>(bytes).Slice(0x00006E0E, PLAYER_CHUNK_LENGTH);
+
+                if (_p1 is null)
+                {
+                    var firstNick = new String([.. p1.Slice(0, 20).ToArray().Select(x => (char)x)]);
+                    _p1 = new Player(p1);
+                }
+
+                return _p1;
+            }
+        }
+
+        public Player P2
+        {
+            get
+            {
+                Span<byte> p2 = new Span<byte>(bytes).Slice(0x00006E0E + PLAYER_TWO_STARTING_INDEX, PLAYER_CHUNK_LENGTH);
+
+                if (_p2 is null)
+                {
+                    var firstNick = new String([.. p2.Slice(0, 20).ToArray().Select(x => (char)x)]);
+                    _p2 = new Player(p2);
+                }
+
+                return _p2;
+            }
+        }
+
+        public GameLobby(byte[] bytes)
+        {
+            this.bytes = bytes;
+        }
+    }
+}
---
+++ src/Game/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BarcodeRevealTool.Game
+{
+    internal class Player
+    {
+
+        public const string UNKNOWN = "UNKNOWN";
+
+        private const short NICKNAME_START_INDEX = 0;
+        private const short BATTLE_TAG_START_INDEX = 82;
+
+        public Regex NickNamePattern = new Regex("(?<nick>[A-Za-z][A-Za-z0-9]{2,20}#[0-9]{3,6})");
+        public Regex BattleTagPattern = new Regex("(?<nick>[A-Za-z][A-Za-z0-9]{2,20}#[0-9]{3,6})");
+
+        private string _battleTag;
+        private string _nickname;
+
+        public string NickName { get; }
+        public string BattleTag { get; }
+
+        public Player(Span<byte> playerSpan)
+        {
+            this.NickName = new String([.. playerSpan.Slice(NICKNAME_START_INDEX, 18).ToArray().Select(c => (char)c)]);
+            this.BattleTag = new String([.. playerSpan.Slice(BATTLE_TAG_START_INDEX, 22).ToArray().Select(c => (char)c)]);
+        }
+
+        public override string ToString()
+        {
+            var nick = NickNamePattern.Match(NickName).Groups["nick"]?.Value;
+            var tag = BattleTagPattern.Match(BattleTag).Groups["nick"]?.Value;
+
+            return new string(
+                @$"[NICK : {nick ?? UNKNOWN}" +
+                $"TAG:{tag ?? UNKNOWN}]"
+            );
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null)
+                throw new ArgumentNullException(nameof(obj));
+
+            Player? other = (Player?)obj;
+
+            bool namesEqual = other.NickName.Equals(NickName);
+            bool tagsEqual = other.NickName.Equals(BattleTag);
+
+            return namesEqual && tagsEqual;
+        }
+    }
+}
